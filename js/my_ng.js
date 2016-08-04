10077b1ab7082f0a88050c8996232cf76089b913
Roman Zavodskikh <roman-zavodskikh@yandex.ru>
2016-08-04 23:46:08 +0300

    Made menu openers more intuitive.

--- js/my_ng.js
+++ js/my_ng.js
@@ -5,19 +5,25 @@ app.controller("myCtrl", function($scope) {
 
     $scope.toggle_white = function () {
         $scope.black_hidden = "none";
+        $scope.black_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         if ($scope.white_hidden !== "block") {
             $scope.white_hidden = "block";
+            $scope.white_menu_hider_style = {backgroundImage: "url('img/menu_close.svg')"};
         } else {
             $scope.white_hidden = "none";
+            $scope.white_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         }
     };
 
     $scope.toggle_black = function () {
         $scope.white_hidden = "none";
+        $scope.white_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         if ($scope.black_hidden !== "block") {
             $scope.black_hidden = "block";
+            $scope.black_menu_hider_style = {backgroundImage: "url('img/menu_close.svg')"};
         } else {
             $scope.black_hidden = "none";
+            $scope.black_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         }
     };
 
