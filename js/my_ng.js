4f27b95cad219b88c5194662bb5d0fa916728472
Roman Zavodskikh <roman-zavodskikh@yandex.ru>
2016-08-05 16:06:20 +0300

    Works in IE, I hope.

--- js/my_ng.js
+++ js/my_ng.js
@@ -1,37 +1,51 @@
-var app = angular.module('myApp', []);
+    var app = angular.module('myApp', []);
 app.controller("myCtrl", function($scope) {
-    $scope.white_hidden = "";
-    $scope.black_hidden = "";
+    $scope.body_style = {};
+    $scope.modal_style = {};
+    $scope.white_menu_style = {};
+    $scope.black_menu_style = {};
 
     $scope.toggle_white = function () {
-        $scope.black_hidden = "none";
+        $scope.black_menu_style = {display: "none"};
         $scope.black_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
-        if ($scope.white_hidden !== "block") {
-            $scope.white_hidden = "block";
+        if ($scope.white_menu_style.display !== "block") {
+            $scope.white_menu_style = {display: "block"};
             $scope.white_menu_hider_style = {backgroundImage: "url('img/menu_close.svg')"};
         } else {
-            $scope.white_hidden = "none";
+            $scope.white_menu_style = {display: "none"};
             $scope.white_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         }
     };
 
     $scope.toggle_black = function () {
-        $scope.white_hidden = "none";
+        $scope.white_menu_style = {display: "none"};
         $scope.white_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
-        if ($scope.black_hidden !== "block") {
-            $scope.black_hidden = "block";
+        if ($scope.black_menu_style.display !== "block") {
+            $scope.black_menu_style = {display: "block"};
             $scope.black_menu_hider_style = {backgroundImage: "url('img/menu_close.svg')"};
         } else {
-            $scope.black_hidden = "none";
+            $scope.black_menu_style = {display: "none"};
             $scope.black_menu_hider_style = {backgroundImage: "url('img/menu.svg')"};
         }
     };
 
     $scope.hide_modal = function () {
-        $scope.modal_display = "none";
+        $scope.modal_style = {display: "none"};
     };
 
     $scope.show_modal = function () {
-        $scope.modal_display = "block";
+        $scope.modal_style = {display: "block"};
     };
+
+    $scope.make_body_grey = function () {
+        $scope.body_style = {background: '#F6F7F9'};
+    }
+
+    $scope.make_body_blue = function () {
+        $scope.body_style = {background: '#0000FF'};
+    }
+
+    $scope.make_body_violet = function () {
+        $scope.body_style = {background: '#8B00FF'};
+    }
 });
