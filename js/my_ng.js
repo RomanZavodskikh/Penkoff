133312aeeb2ed84f47a5d10062e719b23b9789aa
Roman Zavodskikh <roman-zavodskikh@yandex.ru>
2016-08-07 22:21:05 +0300

    And one.

--- js/my_ng.js
+++ js/my_ng.js
@@ -4,6 +4,7 @@ app.controller("myCtrl", function($scope) {
     $scope.modal_style = {};
     $scope.white_menu_style = {};
     $scope.black_menu_style = {};
+    $scope.grey_style = {border: "3px black solid"};
 
     $scope.show_profile = function () {
         $scope.black_menu_style = {display: "block", position: "fixed", height: "100%", zIndex: 1, width: "100%", overflow: "auto",
@@ -33,13 +34,22 @@ app.controller("myCtrl", function($scope) {
 
     $scope.make_body_grey = function () {
         $scope.body_style = {background: '#F6F7F9'};
+        $scope.grey_style = {border: "3px solid black"};
+        $scope.blue_style = {};
+        $scope.purp_style = {};
     }
 
     $scope.make_body_blue = function () {
         $scope.body_style = {background: '#0000FF'};
+        $scope.grey_style = {};
+        $scope.blue_style = {border: "3px solid black"};
+        $scope.purp_style = {};
     }
 
     $scope.make_body_violet = function () {
         $scope.body_style = {background: '#8B00FF'};
+        $scope.grey_style = {};
+        $scope.blue_style = {};
+        $scope.purp_style = {border: "3px solid black"};
     }
 });
